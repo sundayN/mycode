ca698b168c29d59a5d8462ede8240382a807bb4e
SundayN <nyamweno@gmail.com>
2024-01-19 16:19:02 +0000

    dynamic blocks

---
+++ dyn-blocks/phrases/phrases.tf
@@ -0,0 +1,11 @@
+upload_content = [
+  { content = "To infinity, and beyond! (created via our dynamic block)", file = "/terraform/test1.txt", executable = true },
+  { content = "Good news, everyone! (created via our dynamic block)", file = "/terraform/test2.txt", executable = true },
+  { content = "33333  everyone! (created via our dynamic block)", file = "/terraform/test3.txt", executable = true },
+  { content = "4444  news, everyone! (created via our dynamic block)", file = "/terraform/test4.txt", executable = true },
+  { content = "5555 news, everyone! (created via our dynamic block)", file = "/terraform/test5.txt", executable = true },
+
+
+
+]
+
